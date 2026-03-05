526a2cd9d338d78df2c0dd92c703d5fb525e4b82
davi <davicruzramos1@gmail.com>
2026-03-05 00:20:23 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+// Teste para ver se o JavaScript está conectado
+console.log("O cérebro do site está funcionando! 🧠");
+
+// No futuro, colocaremos aqui a lógica de abrir menu no celular, janelas flutuantes, etc.
